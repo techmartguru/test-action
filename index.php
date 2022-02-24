fcadecc0fa1190d9441a07e0fea9d3b5ae6211dd
techmartguru <34309846+techmartguru@users.noreply.github.com>
2022-02-24 16:09:01 +0530

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
   <title>PHP Task</title>
  </head>
  <body>
- <?php echo '<p>My PHP Test Task is Working. And yes, Hello World Version v1.0.0 </p>'; ?>
+ <?php echo '<p>My PHP Test Task is Working. And yes, Hello World Version v1.0.1 </p>'; ?>
  </body>
 </html>
